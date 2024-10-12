090c6727b94eac7d639240bec9a4ee44cb0e1b88
HieuNguyen <hieuoka123@gmail.com>
2024-10-12 21:30:58 +0700

    Cap nhat chat screen

---
+++ navigation/StackNavigator.js
@@ -0,0 +1,48 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from "@react-navigation/stack";
+
+import ChatScreen from "../screens/ChatScreen";
+
+import useAuth from "../hooks/useAuth";
+
+import MessageScreen from "../screens/MessageScreen";
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => {
+  const { user } = useAuth();
+
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+      initialRouteName={"Chat"}
+    >
+      <>
+        <Stack.Group>
+          <Stack.Screen name="Chat" component={ChatScreen} />
+          <Stack.Screen name="Message" component={MessageScreen} />
+        </Stack.Group>
+        <Stack.Group
+          screenOptions={{
+            presentation: "modal",
+            ...TransitionPresets.ModalPresentationIOS,
+          }}
+        ></Stack.Group>
+        <Stack.Group
+          screenOptions={{
+            presentation: "transparentModal",
+            ...TransitionPresets.ModalPresentationIOS,
+          }}
+        ></Stack.Group>
+      </>
+    </Stack.Navigator>
+  );
+};
+
+export default StackNavigator;
